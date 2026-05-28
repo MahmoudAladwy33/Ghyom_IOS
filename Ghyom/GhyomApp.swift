440b3d63edd71df65de039fe3576f37f2d5af714
Mahmoud Aladwy <mahmoudaladwy774@gmail.com>
2026-05-28 12:58:01 -0700

    - first commit

---
+++ Ghyom/GhyomApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhyomApp.swift
+//  Ghyom
+//
+//  Created by Al3dwy on 28/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GhyomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
